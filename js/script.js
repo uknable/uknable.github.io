376f2ca5b43fd4c5b5b84f2f54c4556600b08c0b
uknable <uknable@hotmail.com>
2020-07-27 16:28:08 +1000

    styles tweaks

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-let cards = document.getElementsByClassName("project-cards");
+// let cards = document.getElementsByClassName("project-cards");
 
-let cardNames = ["hearts", "facade", "tiles", "flower"];
-// let cardNames = ["facade", "tiles", "flower", "poem", "earth"];
+// let cardNames = ["hearts", "facade", "tiles", "flower"];
+// // let cardNames = ["facade", "tiles", "flower", "poem", "earth"];
 
+// for (let i=0; i<cards.length; i++) {
 
+//     cards[i].addEventListener("mouseout", (event) => {
+//         event.target.src = "../img/cards/" + cardNames[i] + "_card.gif";
+//     });
+//     cards[i].addEventListener("mouseover", (event) => {
+//         event.target.src = "../img/cards/" + cardNames[i] + "_anim.gif";
+//     });
 
-for (let i=0; i<cards.length; i++) {
-
-    cards[i].addEventListener("mouseout", (event) => {
-        event.target.src = "../img/cards/" + cardNames[i] + "_card.gif";
-    });
-    cards[i].addEventListener("mouseover", (event) => {
-        event.target.src = "../img/cards/" + cardNames[i] + "_anim.gif";
-    });
-
-}
+// }
 
