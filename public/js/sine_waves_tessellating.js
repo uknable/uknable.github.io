46b7a7460a032e8f116a3382e762ad3b76b2e8ef
uknable <uknable@hotmail.com>
2023-10-20 17:56:16 +0300

    update sine_waves_tessellating

--- public/js/sine_waves_tessellating.js
+++ public/js/sine_waves_tessellating.js
@@ -17,15 +17,15 @@
         Using sin/cos are the key to making things look 3D
 */
 
-const cellH = 30;   // 62,  30
-const cellW = 3;    // 7,   3
+const cellH = 60;   // 62,  30, 60
+const cellW = 4.2;    // 7,   3, 4.2
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
         
     noFill();
     stroke('black');
-    strokeWeight(1.4);
+    strokeWeight(1);
 
     pixelDensity(1); // for mobile, pixelDensity(2) is default
 }
@@ -39,8 +39,9 @@ function draw() {
     for (let y=0; y<height; y+=cellH) {
         yCount++;
         
-        const yPhaseNoiseIn = frameCount*0.014 + yCount*14;
-        const yPhase = noise( yPhaseNoiseIn ) * cellH;
+        const yPhaseNoiseIn = frameCount*0.014 - yCount*0.28;
+        const yPhase = noise( yPhaseNoiseIn ) * cellH*0.90;
+        // const yPhase = 0
 
         beginShape();
         vertex( -cellW, y+yPhase );
@@ -49,7 +50,8 @@ function draw() {
         for (let x=0; x<width; x+=cellW) {
             xCount++;
 
-            const yPhaseVar = sin( frameCount*0.028 + xCount*0.056 + yCount*279.9 ) * cellH/4; // !!
+            // const yPhaseVar = 0;
+            const yPhaseVar = sin( frameCount*0.028 + xCount*0.056 + yCount*355.5 ) * cellH/4; // yCount*279.9, yCount*360, yCount*355.5!!
 
             if ( xCount%2 == 0 ) {
                 // Downward half of the wave
